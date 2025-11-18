171684a2209e84015101b5c8d6b193409ab97f96
shuvo2o <eng.shuvo03@gmail.com>
2025-11-18 23:47:17 +0600

    user login done

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\PostController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 Route::post("register", [AuthController::class, 'register']);
+
+Route::post("login", [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get("posts", [PostController::class, 'index']);
+});
